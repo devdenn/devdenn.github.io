e23d52fd6c6dcf4ddc88652135161332e12cb3f0
devdenn <98549100+devdenn@users.noreply.github.com>
2023-01-22 19:19:04 +0100

    change to html5

--- script.ts
+++ script.ts
@@ -3,22 +3,3 @@
      Author .....: Devdenn
      Date .......: 2023-01-22
 */
-
-/* Wait for the DOM */
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("yo");
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
-        });
-    }
-});
