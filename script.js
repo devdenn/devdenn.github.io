c8a590c103a3ef7b9d7bb8faf9ffe7c4250e33ab
devdenn <98549100+devdenn@users.noreply.github.com>
2023-01-22 18:45:33 +0100

    make it more simple

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+     Website  ...: table tennis helper
+     Author .....: Devdenn
+     Date .......: 2023-01-22
+*/
+/* Wait for the DOM */
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("yo");
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            }
+            else {
+                content.style.display = "block";
+            }
+        });
+    }
+});
