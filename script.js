3ac84747f35ccfd1b0e3c56dd3cbabd4a437e03a
devdenn <98549100+devdenn@users.noreply.github.com>
2023-01-22 19:26:01 +0100

    try some fullscreen stuff

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
      Author .....: Devdenn
      Date .......: 2023-01-22
 */
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("load", function () {
+        setTimeout(function () {
+            // This hides the address bar:
+            window.scrollTo(0, 1);
+        }, 0);
+    });
+});
