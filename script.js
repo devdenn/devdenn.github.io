1a4bc59c719904ef22d12da520a6dcafc499d829
devdenn <98549100+devdenn@users.noreply.github.com>
2023-01-23 11:56:32 +0100

    add randomizer

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
     checkbox.addEventListener('change', function () {
         document.body.classList.toggle('dark');
     });
+    document.getElementById('random').addEventListener('click', function () {
+        var random_content = document.getElementById("random-content");
+        var elements = document.getElementsByClassName("min");
+        var rand = randomIntFromInterval(0, elements.length - 1);
+        random_content.innerHTML =
+            "<details class='min'> " + elements[rand].innerHTML +
+                "</details>";
+    });
 });
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
--- script.ts
+++ script.ts
@@ -9,4 +9,21 @@ document.addEventListener("DOMContentLoaded", () => {
     checkbox.addEventListener('change', ()=>{
         document.body.classList.toggle('dark');
     })
+
+    document.getElementById('random').addEventListener('click', ()=>{
+        const random_content = document.getElementById("random-content") as HTMLElement;
+
+        var elements = document.getElementsByClassName("min");
+
+        var rand = randomIntFromInterval(0, elements.length-1);
+
+        random_content.innerHTML =
+            "<details class='min'> " + elements[rand].innerHTML +
+            "</details>";
+    })
 });
+
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
